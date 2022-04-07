741c80577189c786f44ad1c3553c1dcdb111adbb
ExtranoDev <jeomade19@gmail.com>
2022-04-07 23:38:56 +0000

    This is my first C file it prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * This is a file that outputs a string
@@ -8,7 +8,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
+int main(void)
+}
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
