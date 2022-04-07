e8fc0269d0f507ce791fe73f21e784233d899616
ExtranoDev <jeomade19@gmail.com>
2022-04-07 23:50:44 +0000

    Prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This file uses printf to print
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
