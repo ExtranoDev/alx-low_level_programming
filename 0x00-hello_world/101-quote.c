aadd98e1400035e3220bee864bb0bb80c2937ad6
ExtranoDev <jeomade19@gmail.com>
2022-04-08 01:23:45 +0000

    Prints message using putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Successful)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful \" - Dora Korpar, 2015-10-19"
+	int i;
+
+	for (i = 0; i < sizeof(message); i++)
+	{
+		putchar(message[i]);
+	}
+
+	return 1;
+}
