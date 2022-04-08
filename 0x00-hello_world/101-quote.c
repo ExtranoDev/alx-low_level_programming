d9fddfba37222fe5b9889edf9d0f8ed853cabac7
ExtranoDev <jeomade19@gmail.com>
2022-04-08 02:04:21 +0000

    Prints message using putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char mem[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char mem[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i;
 
-	for (i = 0; i < (int) sizeof(mem); i++)
+	for (i = 0; i < ((int) sizeof(mem)) - 1; i++)
 	{
 		putchar(mem[i]);
 	}
