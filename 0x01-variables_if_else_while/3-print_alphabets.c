b4a7749dcc0c4d735c647fb0cf53f3f5a2fa9bf0
ExtranoDev <jeomade19@gmail.com>
2022-04-09 00:38:47 +0000

    Prints all case alphabeths

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x = x + 1;
+	}
+
+	x = 'A';
+
+	while (x <= 'Z')
+	{
+		putchar(x);
+		x = x + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
