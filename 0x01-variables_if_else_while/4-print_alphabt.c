286779bc1feca9c79b47dc6ec0fe6985c64f0150
ExtranoDev <jeomade19@gmail.com>
2022-04-09 00:41:37 +0000

    Prints small case except q and e alphabeths

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'q' || x == 'e')
+			continue;
+		putchar(x);
+		x = x + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
