f69f74340df5acd3f31ccca73e890393d1852d87
ExtranoDev <jeomade19@gmail.com>
2022-04-09 21:53:37 +0000

    Print strange numbers with formatting

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+	int y = 1;
+	
+
+	while (x < 9)
+	{
+		while (y <= 9)
+		{
+			putchar(x % 10 + '0');
+			putchar(y % 10 + '0');
+
+			if (x == 8 && y == 9)
+				break;
+
+			putchar(',');
+			putchar(' ');
+			y = y + 1;
+		}
+
+		x = x + 1;
+		y = x + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
