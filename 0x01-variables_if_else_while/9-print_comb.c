8d57e5dff8d6a6ccd053fe01d2572277c021bc81
ExtranoDev <jeomade19@gmail.com>
2022-04-09 01:27:36 +0000

    Prints all psossible combinationof sigle digit number with some formatting

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar(a%10 + '0');
+		putchar(',');
+		putchar(' ');
+		a = a + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
