aefa36156eda2149504b8f651a646f0d0e2d3ccd
ExtranoDev <jeomade19@gmail.com>
2022-04-13 02:32:22 +0000

    Prints to 98 with any number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Make sure it prints 98 from number backward or forward
+ *@i: first int input
+ *
+ */
+void print_to_98(int i)
+{
+	if (i < 98)
+	{
+		while ( i <= 98)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				break;
+			}
+
+			printf("%d, ", i);
+			i++;
+		}
+	}
+	else if (i > 98)
+	{
+		while (i >= 98)
+		{
+			if (i == 98)
+			{
+				printf("%d", i);
+				break;
+			}
+
+			printf("%d, ", i);
+			i--;
+		}
+	}
+	else
+	{
+		printf("%d", i);
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _abs(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
 int print_last_digit(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
