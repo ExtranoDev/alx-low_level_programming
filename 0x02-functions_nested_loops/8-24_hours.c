34bc9808e9f2c3892b8b955b0f54d831247f42ef
ExtranoDev <jeomade19@gmail.com>
2022-04-13 01:19:11 +0000

    Prints Jack Bauer 24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints Jack Bauer hours
+ */
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = 0; w < 3; w++)
+	{
+		for (x = 0; x < 4; x++)
+		{
+			for (y = 0; y < 7; y++)
+			{
+				for (z = 0; z < 10; z++)
+				{
+					_putchar(w + '0');
+					_putchar(x + '0');
+					_putchar(':');
+					_putchar(y + '0');
+					_putchar(z + '0');
+				}
+			}
+		}
+	}
+}
