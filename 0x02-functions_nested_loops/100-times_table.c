9102d0579ea8205e3b8b2155fab6046451f77533
ExtranoDev <jeomade19@gmail.com>
2022-04-13 21:11:06 +0000

    Prints times table of n numbers

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints a time table of n inputed numbers
+ */
+void print_times_table(int n)
+{
+	int x, y = 0, z, temp1, temp2;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (z = 0; z <= n; z++)
+		{
+			temp1 = y / 100;
+			temp2 = y / 10
+
+			if (temp1 != 0)
+				_putchar(temp1 + '0');
+
+			if (temp2 != 0)
+				_putchar(temp2 + '0');
+
+			_putchar(y % 10 + '0');
+
+			if (z == 9)
+				break;
+
+			_putchar(',');
+			y = y + x;
+
+			if (y < 100)
+				_putchar(' ');
+			if (y < 10)
+				_putchar(' ');
+
+			_putchar(' ');
+		}
+
+		_putchar('\n');
+		y = 0;
+	}
+}
