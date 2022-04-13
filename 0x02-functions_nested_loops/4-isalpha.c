9b72fc3dabced56d7982b3244cfb08abbf10d342
ExtranoDev <jeomade19@gmail.com>
2022-04-13 00:07:37 +0000

    Checks if input is an Uppercase letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a letter is lower case
+ *@c: letter to be checked
+ *
+ * Return: Return depends on c
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
