1537f0dd75414ab09cecd31af4a8840cd73db399
ExtranoDev <jeomade19@gmail.com>
2022-04-16 06:56:59 +0000

    Prints num from 0 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * more_numbers - print most digit numbers
@@ -12,10 +13,14 @@ void more_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar((j / 10) + '0');
+		{
+			if (j > 9)
+				_putchar((j /10) + '0');
 
+			_putchar((j % 10) + '0');
+		}
+
+		putchar('\n');
 		j = 0;
 	}
-
-	_putchar('\n');
 }
