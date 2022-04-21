c7339d62280c82e72e465a9fb5b87695fb7e4d12
ExtranoDev <jeomade19@gmail.com>
2022-04-21 16:48:20 +0000

    Reverse string using pointters

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string using pointers
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i;
+	char x[10];
+
+	for (i = 0; s[i] != '\0'; ++i)
+		_putchar(s[i]);
+
+	i--;
+
+	for ( ; i >= 0; i--)
+		x[9 - i] = s[i];
+
+	*s = x[10:];
+	_putchar('\n');
+}
