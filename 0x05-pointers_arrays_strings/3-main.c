d5790514be0b74158d762b732957ad76ba59fbca
ExtranoDev <jeomade19@gmail.com>
2022-04-21 01:25:17 +0000

    Main file for fourth exercise

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char *str;
 
-	str = "I do not fear the lack of them - Isaac Asimov";
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 	_puts(str);
 	return (0);
 }
