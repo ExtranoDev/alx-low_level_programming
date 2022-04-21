0d32854f3e1355e39d3ff5b7690d373e5bf0fb48
ExtranoDev <jeomade19@gmail.com>
2022-04-21 15:55:24 +0000

    Prints strings in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints stuffs in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+	}
+
+	for ( ; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
