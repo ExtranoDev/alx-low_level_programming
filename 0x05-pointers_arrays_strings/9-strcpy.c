4d08064f47243639ba5d75d34ca4cec5a5906ad6
ExtranoDev <jeomade19@gmail.com>
2022-04-22 02:46:29 +0000

    Copies string from one point to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies string from one var to another
+ * @dest: destination
+ * @src: Source
+ *
+ * Return: Destination
+ */
+void *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
