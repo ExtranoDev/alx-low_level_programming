676b66cabfd0c91768bda732cab14f93f42f6a7a
ExtranoDev <jeomade19@gmail.com>
2022-04-24 23:03:48 +0000

    Changes lower case string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * string_toupper - change str to upper
+ * *string_toupper - change str to upper
  * @s: string
+ *
+ * Return: s
  */
-char string_toupper(char *s)
+char *string_toupper(char *s)
 {
 	int x;
-	
+
 	for (x = 0; s[x] != '\0'; x++)
 	{
 		if (s[x] > 96 && s[x] < 123)
