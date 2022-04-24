3fd4e4e92ada1de670e2b0fa39454eddadd0d850
ExtranoDev <jeomade19@gmail.com>
2022-04-24 21:42:57 +0000

    Main file for third exercise

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
+ * _strncpy - concatenates two strings
  * @dest: destination string
  * @src: source string
  * @n: number of string(s) to be copied
@@ -18,8 +18,8 @@ char *_strncpy(char *dest, char *src, int n)
 		i++;
 	}
 
-	for ( ; n > i; n++)
-		dest[x] = '\0';
+	for ( ; n > i; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
