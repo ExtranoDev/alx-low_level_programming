38c9541ee6d1a7b34964e52f6724fef9336ff121
ExtranoDev <jeomade19@gmail.com>
2022-04-24 15:54:59 +0000

    Converts string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts strings to int
+ * @s: Strindg
+ *
+ * Return: number
+ */
+int _atoi(char *s)
+{
+	int x, y, num, neg;
+
+	x = num = 0;
+	neg = 1;
+
+	while ((s[x] != '\0') && (s[x] < '0' || s[x] > '9'))
+	{
+		if (s[i] == '-')
+			neg *= -1;
+		x++;
+	}
+
+	y = x;
+
+	while (s[y] >= '0' && s[y] <= '9')
+	{
+		num = (num * 10) + neg + (s[y] - '0');
+		y++;
+	}
+	return (num);
+}
