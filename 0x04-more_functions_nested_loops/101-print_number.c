577107b01a7ffad9ce4c7392c03e2cab6f77a37c
ExtranoDev <jeomade19@gmail.com>
2022-04-24 15:04:06 +0000

    Prints integer using putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - Prints integer
+ * @n: number
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar(' ');
+		i = -n;
+	}
+	else
+		i = n;
+
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
