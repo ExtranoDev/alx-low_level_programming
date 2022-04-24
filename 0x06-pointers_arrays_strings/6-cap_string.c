ed3c8a58b3eb864035f530605102c6dfdb559481
ExtranoDev <jeomade19@gmail.com>
2022-04-24 23:32:45 +0000

    Capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,14 +8,29 @@
  */
 char *cap_string(char *s)
 {
-	int x;
+	int x, y, sens = 0;
+	char nt[] = ",;.!?(){}\n\t\" ";
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		if (s[x] == 32)
+		if (s[0] > 96 && s[0] < 123)
+			sens = 1;
+		for (y = 0; nt[y] != '\0'; y++)
 		{
-			if (s[x + 1] > 96 && s[x + 1] < 123)
-				s[x + 1] -= 32;
+			if (nt[y] == s[x])
+				sens = 1;
+		}
+		if (sens)
+		{
+			if (s[x] > 96 && s[x] < 123)
+			{
+				s[x] -= 32;
+				trigger = 0;
+			}
+			else if (s[x] > 64 && s[x] <91)
+				sens = 0;
+			else if (s[x] > 47 && s[x] < 58)
+				sens = 0;
 		}
 	}
 	return (s);
