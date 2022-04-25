fb1174a0c378858721e452aa7c9ed3d4e5674cea
ExtranoDev <jeomade19@gmail.com>
2022-04-25 20:42:24 +0000

    Does 1337 encryption

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,21 @@
  */
 char *leet(char *s)
 {
-	int x;
+	int x, y;
+	char low[] = "aeoti";
+	char up[] = "AEOTL";
+	char nums[] = "43071";
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		if (s[x] == 97 || s[x] == 65)
-			s[x] = 4 + '0';
-		else if (s[x] == 101 || s[x] == 69)
-			s[x] = 3 + '0';
-		else if (s[x] == 79 || s[x] == 111)
-			s[x] = 0 + '0';
-		else if (s[x] == 84 || s[x] == 116)
-			s[x] = 7 + '0';
-		else if (s[x] == 76 || s[x] == 108)
-			s[x] = 1 + '0';
-		else
-			continue;
+		for (y = 0; low[y] != '\0' && up[y] != '\0'; y++)
+		{
+			if (s[x] == low[y] || s[x] == up[y])
+			{
+				s[x] = nums[y];
+				break;
+			}
+		}
 	}
 	return (s);
 }
