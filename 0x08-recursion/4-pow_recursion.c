a80f700ea7b53a9372aed697a299a81ec53a43c2
ExtranoDev <jeomade19@gmail.com>
2022-04-29 02:35:56 +0000

    Find recursion recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - finds the power of a num to another num
+ * @x: base number
+ * @y: power
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y >= 1)
+		y -= 1;
+		return (x);
+	return (_pow_recursion(x * y));
+}
