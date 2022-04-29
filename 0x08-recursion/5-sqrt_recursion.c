56f96fc9ae9b4893677642babe42c54f3579c37d
ExtranoDev <jeomade19@gmail.com>
2022-04-29 03:16:18 +0000

    File for sqrt using recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds sqrt of num
+ * @n: number
+ * Return: sqrt(n)
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (i == 1)
+		return (i)
+
+}
