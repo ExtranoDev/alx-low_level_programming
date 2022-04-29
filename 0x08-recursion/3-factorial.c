89d91227015df7632ae26b2970111d727cc462de
ExtranoDev <jeomade19@gmail.com>
2022-04-29 02:15:56 +0000

    Finds factorial using recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,9 @@
  */
 int factorial(int n)
 {
-	int fact;
-
 	if (n == 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	fact = n * factorial(n - 1);
-	return (fact);
+	return (n * factorial(n - 1));
 }
