f9a73a053b968ed38b6ca26fc3ae53d216c3bdc9
ExtranoDev <jeomade19@gmail.com>
2022-04-29 01:56:29 +0000

    Prints len of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,13 +5,14 @@
  * @s: string
  * Return: length of string
  */
+int n;
+
 int _strlen_recursion(char *s)
 {
-	int n = 0;
-
 	if (*s == '\0')
-		return n + 1;
+		return n;
 	else
 		n += 1;
 	_strlen_recursion(s += 1);
+	return n;
 }
