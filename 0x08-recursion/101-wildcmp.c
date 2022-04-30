25d110728b8432e23ffc4612ebac84b56c9e12e5
ExtranoDev <jeomade19@gmail.com>
2022-04-30 00:47:17 +0000

    Wildcard comparison

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares strings wild char option
+ * @s1: source string
+ * @s2: destination string
+ * Return: palindrome status
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s1 == *s2);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0)
+}
