df2c4111181edf90bd5dc6489b11e3ebd8cdfbf7
ExtranoDev <jeomade19@gmail.com>
2022-05-09 18:02:10 +0000

    main file for third exercise

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,3 +2,5 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int countLen(char *s);
+char *rrconcat(char *s1, char *s2, int n1, int n2);
