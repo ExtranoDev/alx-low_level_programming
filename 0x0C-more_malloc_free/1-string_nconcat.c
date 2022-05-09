7e9b68d01079c37a5bf2b1a936e0bcaacbfc55ca
ExtranoDev <jeomade19@gmail.com>
2022-05-09 22:23:20 +0000

    Concatenate string with condition

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - return allocated memory
+ * @s1: string
+ * @s2: string
+ * @n: number
+ *
+ * Return: string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int l1, l2, i, j = 0;
+
+	if (s2 != NULL)
+	{
+		for (l2 = 0; s2[l2] != '\0'; l2++)
+			;
+	}
+
+	if (s1 != NULL)
+	{
+		for (l1 = 0; s1[l1] != '\0'; l1++)
+			;
+	}
+
+	l2++;
+	if (n >= l2)
+		n = l2;
+
+	str = malloc((l1 + l2 + 1) * sizeof(char));
+	
+	if (str == NULL)
+		return ('\0');
+
+	if (l1 != 0)
+	{
+		for (i = 0; (str[j] = s1[i]) != '\0'; ++i, ++j)
+			;
+	}
+	if (l2 != 0)
+	{
+		for (i = 0; i < n; ++i, ++j)
+			str[j] = s2[i];
+	}
+	return (str);
+}
