d6aef3e0bb8c64278ad5a03869861f159df065bd
ExtranoDev <jeomade19@gmail.com>
2022-05-09 18:01:47 +0000

    Marge strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - concatinates strings
+ * @s1: string
+ * @s2: string
+ *
+ * Return: string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i1 = 0, i2 = 0;
+
+	if (s1 == NULL)
+	{
+		i1 = -1;
+		s1 = "";
+	}
+	else if (s2 == NULL)
+	{
+		i2 = -1;
+		s2 = "";
+	}
+	if (i1 != -1)
+	{
+		i1 = countLen(s1);
+	}
+	if (i2 != -1)
+	{ 
+		i2 = countLen(s2);
+	}
+	return (rrconcat(s1, s2, i1, i2));
+}
+
+/**
+ * rconcat - concatenate string
+ * @s1: first string
+ * @s2: second string
+ * @n1: length of first string
+ * @n2: length of second string
+ */
+char *rrconcat(char *s1, char *s2, int n1, int n2)
+{
+	char *i;
+	int count = 0, temp;
+
+	if (n1 == -1)
+		n1 = 0;
+	else if (n2 == -1)
+		n2 = 0;
+
+	temp = n1 + n2 + 1;
+	i = malloc(temp * sizeof(char));
+	if (i == NULL)
+		return ('\0');
+	for (temp = 0; (i[count] = s1[temp]) != '\0'; ++temp, ++count)
+	       ;
+	for (temp = 0; (i[count] = s2[temp]) != '\0'; ++temp, ++count)
+	;	
+	i[count] = '\0';
+	printf("%s\n", i);
+	return (i);
+}
+
+/**
+ * countLen - count length of a string
+ * @s: string
+ * Return: length
+ */
+int countLen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+		;
+	return (n);
+}
