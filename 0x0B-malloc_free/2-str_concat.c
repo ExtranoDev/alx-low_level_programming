186ac035cd7b3a363d9d00720e798b0df3af3580
ExtranoDev <jeomade19@gmail.com>
2022-05-09 18:08:15 +0000

    Merge strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,18 +28,19 @@ char *str_concat(char *s1, char *s2)
 		i1 = countLen(s1);
 	}
 	if (i2 != -1)
-	{ 
+	{
 		i2 = countLen(s2);
 	}
 	return (rrconcat(s1, s2, i1, i2));
 }
 
 /**
- * rconcat - concatenate string
+ * rrconcat - concatenate string
  * @s1: first string
  * @s2: second string
  * @n1: length of first string
  * @n2: length of second string
+ * Return: concatenated string
  */
 char *rrconcat(char *s1, char *s2, int n1, int n2)
 {
@@ -56,9 +57,9 @@ char *rrconcat(char *s1, char *s2, int n1, int n2)
 	if (i == NULL)
 		return ('\0');
 	for (temp = 0; (i[count] = s1[temp]) != '\0'; ++temp, ++count)
-	       ;
+		;
 	for (temp = 0; (i[count] = s2[temp]) != '\0'; ++temp, ++count)
-	;	
+		;	
 	i[count] = '\0';
 	printf("%s\n", i);
 	return (i);
