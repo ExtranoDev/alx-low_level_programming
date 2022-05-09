b5359639683ba554a6ea921e6285186f5f7b0f1f
ExtranoDev <jeomade19@gmail.com>
2022-05-09 18:31:30 +0000

    Merge strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,28 +13,12 @@ char *str_concat(char *s1, char *s2)
 {
 	int i1 = 0, i2 = 0;
 
-	if (s1 == NULL)
-	{
-		i1 = -1;
-		s1 = "";
-	}
-	else if (s2 == NULL)
-	{
-		i2 = -1;
-		s2 = "";
-	}
-	else if(i1 == -1 && i2 == -1)
-		return ('\0');
 	if (i1 != -1)
 	{
-		if (i2 == -1)
-			return (s2);
 		i1 = countLen(s1);
 	}
 	if (i2 != -1)
 	{
-		if (i1 == -1)
-			return (s1);
 		i2 = countLen(s2);
 	}
 
@@ -58,10 +42,10 @@ char *rrconcat(char *s1, char *s2, int n1, int n2)
 	i = malloc(temp * sizeof(char));
 	if (i == NULL)
 		return ('\0');
-	if (n1 != -1)
+	if (n1 != 0)
 		for (temp = 0; (i[count] = s1[temp]) != '\0'; ++temp, ++count)
 			;
-	if (n2 != -1)
+	if (n2 != 0)
 		for (temp = 0; (i[count] = s2[temp]) != '\0'; ++temp, ++count)
 			;
 	i[count] = '\0';
