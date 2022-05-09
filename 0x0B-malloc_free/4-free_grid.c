0257df3cffd37531d35d34890aad1c02027fd692
ExtranoDev <jeomade19@gmail.com>
2022-05-09 20:35:52 +0000

    Frees dynamic 2D arrays

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_grid - free 2D array
+ * @grid: array
+ * @height: height of array
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; ++i)
+		free(grid(i));
+	free(grid)
+}
