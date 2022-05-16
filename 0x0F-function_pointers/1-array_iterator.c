6416117577c39ee3f674607380f5c480d18a3aac
ExtranoDev <jeomade19@gmail.com>
2022-05-16 02:44:16 +0000

    Iterates a function on function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * array_iterator - function function iterator
@@ -8,7 +9,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (action && array)
 	{
