c7bf3194ba181b8c92a1e1dc616ab91df04964b3
ExtranoDev <jeomade19@gmail.com>
2022-05-16 03:30:33 +0000

    Operation function selector

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: first int
+ * @b: second int
+ * Return: sum of two int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction
+ * @a: first int
+ * @b: second int
+ * Return: subtract of two int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: first int
+ * @b: second int
+ * Return: multiply two int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: first int
+ * @b: second int
+ * Return: division two int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: first int
+ * @b: second int
+ * Return: remainder division between two int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
