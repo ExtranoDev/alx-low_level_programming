f0c4f70abd463c32bcf72ad3e58b07dabe0b46ca
ExtranoDev <jeomade19@gmail.com>
2022-05-16 02:21:58 +0000

    Prints inputed string

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,10 @@
  * print_name - prints inputed name
  * @name: name string
  * @f: function
+<<<<<<< HEAD
  * Return: nothing
+=======
+>>>>>>> d1c6a33219d06d1357e7b5912ae82d86ea180ad8
  */
 void print_name(char *name, void (*f)(char *))
 {
