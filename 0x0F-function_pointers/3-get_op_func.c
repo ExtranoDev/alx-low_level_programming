be795099709a906d3a0dcb7ab5ff849c2af2c17e
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-15 21:17:25 -0700

    Updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	for (i = 0; ops[i].op && (*(ops[i].op) != *s); i++)
-		;
+	while(ops[i].op && (*(ops[i].op) != *s))
+		i++;
 	return (ops[i].f);
 }
