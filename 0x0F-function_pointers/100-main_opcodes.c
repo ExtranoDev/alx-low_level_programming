87dd1b9c702efb5d118bcc5aeee1651ffcee3427
ExtranoDev <jeomade19@gmail.com>
2022-05-16 04:43:02 +0000

    Prints opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	char *x;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	i = atoi(argv[1]);
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	x = (char *)&main;
+
+	for (j = 0; j < i; j++)
+	{
+		printf("%.2hhx", x[j]);
+		if (j < i - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
