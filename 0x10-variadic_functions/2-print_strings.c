67e6e69564505e5d38a75816ff596988609517b0
ExtranoDev <jeomade19@gmail.com>
2022-05-18 03:30:58 +0000

    Print strings only

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings inly
+ * @separator: seperates output
+ * @n: number of integers
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+	char *str;
+	unsigned int i;
+
+	va_start(vl, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(i, char *);
+		if (!str)
+			printf("(nil)");
+		else
+			printf("%d", str);
+
+		if (i != (n - 1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(vl);
+}
