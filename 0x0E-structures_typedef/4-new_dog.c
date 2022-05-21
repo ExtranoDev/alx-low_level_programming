51180ab37754ca3e5214fe08dcef9ebb0603c708
ExtranoDev <jeomade19@gmail.com>
2022-05-21 16:58:01 +0000

    Creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j;
 	dog_t *my_dog;
 
-	if (name == NULL || owner == NULL)
+	if (!name || !owner)
 		return (NULL);
-
-
 	my_dog = malloc(sizeof(dog_t));
-	my_dog->name = name;
+	if (!my_dog)
+		return (NULL);
+	for (i = 0; name[i]; i++)
+		;
+	i++;
+	my_dog->name = malloc(i * sizeof(char));
+	if (!my_dog->name)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+		my_dog->name[j] = name[j];
 	my_dog->age = age;
-	my_dog->owner = owner;
+	for (i = 0; owner[i]; i++)
+		;
+	i++;
+	my_dog->owner = malloc(i * sizeof(char));
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+		my_dog->owner[j] = owner[j];
 	return (my_dog);
 }
-
