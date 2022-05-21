5ab1afa583c073f47018d5dc351adee0515bda41
ExtranoDev <jeomade19@gmail.com>
2022-05-21 15:31:19 +0000

    Header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOGSTRUCT
+#define DOGSTRUCT
+
+/**
+ * dog - 
+ * @name: dog's name
+ * @owner: the owner
+ * @age: dog's age
+ * Description: a struct with dog name, age, and owner's name
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
