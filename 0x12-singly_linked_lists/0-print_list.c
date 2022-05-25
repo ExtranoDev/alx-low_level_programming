dc0a627207af59d89f56d025429165499781aa01
ExtranoDev <jeomade19@gmail.com>
2022-05-25 01:56:38 +0000

    First files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print elements of linkedlist
+ * @h: list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nbr_nodes = 0;
+	const list_t *pstr = h;
+
+	while (psrt)
+	{
+		printf("[%d] %s\n", pstr->len, pstr->str != NULL ? current->str : "(nil)");
+		pstr = pstr->next;
+		nbr_nodes++;
+	}
+	return (nbr_nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Dir for linked list exercise
