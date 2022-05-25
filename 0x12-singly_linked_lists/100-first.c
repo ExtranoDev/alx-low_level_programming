9e5c232719476dcfec679883667d5727ee8dd144
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-25 03:36:16 +0100

    Updated for betty-style check

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,10 @@
 
 /**
  * premain - prints string before main
+ * Return: nothing
  */
 void __attribute__ ((constructor)) premain()
 {
-  printf(
-    "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
-  );
+  printf("You're beat! and yet, you must allow,\n);
+  printf("I bore my house upon my back!\n");
 }
