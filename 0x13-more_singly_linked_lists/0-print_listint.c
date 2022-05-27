ce17e72d70eb22a2589d724616da80290457f91e
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-26 20:37:51 -0700

    Prints elements in a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - print elements of single linkedlist
+ * @h: list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num_n = 0;
+	const listint_t *curNode = h;
+
+	while (curNode)
+	{
+		printf("%i\n", curNode->n);
+		curNode = curNode->next;
+		num_n++;
+	}
+	return (num_n);
+}
