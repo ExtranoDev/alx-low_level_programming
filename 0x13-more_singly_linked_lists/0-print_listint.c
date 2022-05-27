991489fb6cbd0c645944db8cf2e0736964a286df
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-05-26 20:41:18 -0700

    Updated for requirement

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,7 +12,7 @@ size_t print_listint(const listint_t *h)
 	size_t num_n = 0;
 	const listint_t *curNode = h;
 
-	while (curNode)
+	while (curNode != NULL)
 	{
 		printf("%i\n", curNode->n);
 		curNode = curNode->next;
