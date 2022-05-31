45ad99a734ee16f6515ce3b53b260855932b7bce
ExtranoDev <jeomade19@gmail.com>
2022-05-31 03:48:10 +0000

    Second exercise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,45 +8,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int tmpHold, j, mul;
-	int i;
-	char bin[1000];
+	if (n > 1)
+		print_binary(n >> 1);
 
-	i = j = mul = 0;
-	tmpHold = n;
-
-	if (!n)
-		_putchar('0');
-	else if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-
-	while (1)
-	{
-		if (1 << i <= (int)(n))
-			mul = 1 << i;
-		else
-			break;
-		i++;
-	}
-	--i;
-	while (i >= 0)
-	{
-		if (tmpHold >= mul)
-		{
-			tmpHold -= mul;
-			bin[j] = '1';
-		}
-		else
-			bin[j] = '0';
-		j++;
-		i = i - 1;
-		mul = 1 << i;
-	}
-
-	bin[j] = '\0';
-	for (j = 0; bin[j] != '\0'; j++)
-		_putchar(bin[j]);
+	_putchar((n & 1) + '0');
 }
