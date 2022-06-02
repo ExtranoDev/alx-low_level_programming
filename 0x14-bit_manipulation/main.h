dc1264d363457d436d8f530caa52732a11e7c5b3
ExtranoDev <jeomade19@gmail.com>
2022-06-02 10:50:04 +0000

    Update for setting bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef BITMAN
 #define BITMAN
 
+#define BYTE_CONST 8
+
 #include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
@@ -9,5 +11,6 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int get_endianness(void);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
