7b3d147ac836898ffc0dece0251c5d6fe9188f16
ExtranoDev <jeomade19@gmail.com>
2022-06-02 10:16:58 +0000

    Updated for getting bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int get_endianness(void);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
