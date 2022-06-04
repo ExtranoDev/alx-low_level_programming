7062bc0c0b75153b77f87c1700a479d1ad6f750b
ExtranoDev <jeomade19@gmail.com>
2022-06-04 21:24:31 +0000

    Create a file and write to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
-		       ;
+			;
 		r_letter = write(fd, text_content, i);
 		if (r_letter == -1)
 			return (-1);
