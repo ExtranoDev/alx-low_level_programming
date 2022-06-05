731cd508b55a7d6f8078449741dc180013219577
ExtranoDev <jeomade19@gmail.com>
2022-06-05 18:06:07 +0000

    Append text to existing file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,14 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		return (1);
-	fd = open(filename, O_WRONLY | O_APPND);
+	fd = open(filename, O_APPEND | O_WRONLY);
 
 	if (fd == -1)
 		return (-1);
 
-	for (i = 0; text_content = '\0'; i++)
+	for (i = 0; text_content[i] == '\0'; i++)
 		;
-	r_letter = (filename, text_content, i);
+	r_letter = write(fd, text_content, i);
 	if (r_letter == -1)
 		return (-1);
 	close(fd);
