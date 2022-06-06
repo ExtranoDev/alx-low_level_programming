f812910c96c414033683347fc462448ed9283323
ExtranoDev <jeomade19@gmail.com>
2022-06-06 13:14:31 +0000

    Corrected to append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,16 +16,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
-		return (1);
-
-	for (i = 0; text_content[i] == '\0'; i++)
-		;
-	r_letter = write(fd, text_content, i);
-	if (r_letter == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i] == '\0'; i++)
+			;
+		r_letter = write(fd, text_content, i);
+		if (r_letter == -1)
+			return (-1);
+	}
 	close(fd);
-	if (r_letter == -1)
-		return (-1);
 	return (1);
 }
