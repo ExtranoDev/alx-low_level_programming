6c316a804a4d64c8d595cc769fc5501ee335b80c
ExtranoDev <jeomade19@gmail.com>
2022-07-09 14:18:03 +0000

    Add new list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
-	*head = new;
+	new->next = *head->prev
+	*head->prev = new;
 	return (*head);
 }
